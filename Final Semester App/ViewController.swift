5e63bbccb184ffcd1a5b033b30368460d629014e
Calen Benson <cbens205@d127.org>
2021-12-14 15:18:58 -0600

    2nd commit

--- Final Semester App/ViewController.swift
+++ Final Semester App/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nightImageView: UIImageView!
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      
     }
 
 
